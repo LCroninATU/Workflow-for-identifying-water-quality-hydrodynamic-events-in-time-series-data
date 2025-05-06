41a85a7b560f553443afbe398ab45255143e50d6
LCron <165794851+LCroninATU@users.noreply.github.com>
2025-05-06 15:10:27 +0100

    update scripts to match markdown

--- 08 Manually compare events to CANARY events/Scripts/Compare manually identified Owenmore turbidity events to positive CANARY event.R
+++ 08 Manually compare events to CANARY events/Scripts/Compare manually identified Owenmore turbidity events to positive CANARY event.R
@@ -5,124 +5,135 @@ library(here)
 library(data.table) 
 library(dplyr)
 
-# Compare the manually identified events file ('OW061024_051124C_NoOutRange_ManVer_TurbEvent.csv') to each of the 16 algorithms tested for each BED window and event time out (ETO) in CANARY i.e total of 96 files
 
-## Load the manually identified events file ('OW061024_051124C_NoOutRange_ManVer_TurbEvent.csv')
-
-#cIAN - THIS WON'T LOAD AND I CAN'T FIGURE OUT WHY! IT's GIVING ME  AN ERROR THAT IT CAN'T FIND THE COLUMN NAMES.  
-#SO I PUT THE FILE IN THE WORKING DIRECTORY AS A TEMPORARY WORKAROUND (WHICH CAN IMPORT NO PROBLEM!) THAT I CAN EDIT LATER ONCE I FIGURE OUT THE ISSUE
-OW061024_051124C_ManVer_TurbEvent<-fread("06 Manually Identify Potential Water Quality Events/OW061024_051124C_NoOutRange_ManVer_TurbEvent.csv",header=T, 
-                                         stringsAsFactors = F, select = c("DateTime","Turbidity","Verified_OW_Event_Alarm"))
-
-#OW061024_051124C_ManVer_TurbEvent<-fread("OW061024_051124C_NoOutRange_ManVer_TurbEvent.csv",header=T, 
- #                                        stringsAsFactors = F, select = c("DateTime","Turbidity","Verified_OW_Event_Alarm"))
-str(OW061024_051124C_ManVer_TurbEvent)
-#Change DateTime from chr to Date 
-OW061024_051124C_ManVer_TurbEvent$DateTime<-as.POSIXct(OW061024_051124C_ManVer_TurbEvent$DateTime, format = "%d/%m/%Y %H:%M", tz="UTC")
-str(OW061024_051124C_ManVer_TurbEvent)
-# and rename to TIME_STEP to match the Owenmore CANARY File
-library(tidyverse)
-OW061024_051124C_ManVer_TurbEvent<- OW061024_051124C_ManVer_TurbEvent%>% rename(TIME_STEP = DateTime)
+# This section of the code combines the manually identified events by the user with the output for each of the 96 algorithm files produced by CANARY into one dataframe, creating 96 new .csv files.  The files are saved into six directories (folders) in the same file format as the CANARY output with the file name structure: sitedate_algorithm type_algorithm details_algorithm number.  These files are saved in the directory associated with that data which is specified by the user by amending the code for the identifier in section 8.12.  In this example, the data to be processed was placed in the directory called Data1 and the identifier set as follows #data_set_identifier<-"Data1/".  Where mutiple datasets are to be processed the user places each dataset in it's own directory, e.g. Data1, Data2, Data3 etc and updates the code for data_set_identifier when running each dataset.  The code automatically creates a directory in Output_Files for each dataset specified and ran through the code e.g. Output_Files/Data1, Output_Files/Data2 etc. 
+# The output for step 8 comparing manually identified and CANARY events is saved in 08 Manually compare events to CANARY events/Output_files.
 
 
-
-
-## Load the Owenmore CANARY file From 07 EPA CANARY Event Detection Folder
-
-CANARYOwenmoreBED6ET0.89063Alg1<-fread("07 EPA CANARY Event Detection/Owenmore_LPCF_BED6_ET0.89063/Owenmore_LPCF_Var_HWV01.OW061024_051124C_ET0.89063-details-alg_1.csv", header=T, stringsAsFactors = F,
-                          select = c("TIME_STEP","Contrib_{Turbidity}"))
+# Specify the directory where the "manually verified event data" in located.
  
-str(CANARYOwenmoreBED6ET0.89063Alg1)
+# The path for this directory should be given relative to the project directory.
+# Set the top directory for this step
+ManVer_file_top_directory<-"06 Manually Identify Potential Water Quality Events/Output_Files/"
 
-#Change TIME_STEP from chr to Date 
-CANARYOwenmoreBED6ET0.89063Alg1$TIME_STEP<-as.POSIXct(CANARYOwenmoreBED6ET0.89063Alg1$TIME_STEP, format = "%d/%m/%Y %H:%M", tz="UTC")
-# rename column
-CANARYOwenmoreBED6ET0.89063Alg1<- CANARYOwenmoreBED6ET0.89063Alg1%>% rename(CANARY_Contrib_Turbidity = "Contrib_{Turbidity}")
-str(CANARYOwenmoreBED6ET0.89063Alg1)
+# This identifier is used to select the "manually verified event dataset" to be processed.
+data_set_identifier<-"Data1/"
 
+# Sets up the full path to the "manually verified event data".
+ManVer_file_directory<-paste0(ManVer_file_top_directory,data_set_identifier)
+(ManVer_file<-list.files(ManVer_file_directory,pattern='ManVer_TurbEvent'))
+ManVer_file_full_path<-paste0(ManVer_file_directory,ManVer_file)
 
-# Change any negative values in CANARYOwenmoreAlg1 Contrib_{Turbidity} to absolute values to facilitate matching query
-CANARYOwenmoreBED6ET0.89063Alg1$`CANARY_Contrib_Turbidity`<-abs(CANARYOwenmoreBED6ET0.89063Alg1$`CANARY_Contrib_Turbidity`)
+# Load the "manually verified event data" and format the dataframe.
+ 
+# Load the "manually verified event data".
+ManVer_TurbEvent<-fread(ManVer_file_full_path,header=T,stringsAsFactors = F, select = c("DateTime","Turbidity","Verified_OW_Event_Alarm"))
+str(ManVer_TurbEvent)
 
-# The number of timesteps in the Manually identified turbidity file must not be longer than the CANARY file or NA will be inserted.
-# Show the last timestamp in the CANARY File
-last_timestep_CANARY<-max(CANARYOwenmoreBED6ET0.89063Alg1$TIME_STEP)
+# Change DateTime from chr to Date (as.POSIXct stores both a date and time with an associated time zone)
+ManVer_TurbEvent$DateTime<-as.POSIXct(ManVer_TurbEvent$DateTime, format = "%d/%m/%Y %H:%M", tz="UTC")
+str(ManVer_TurbEvent)
 
+# Rename to TIME_STEP to match the Owenmore CANARY File
+ManVer_TurbEvent<- ManVer_TurbEvent%>% rename(TIME_STEP = DateTime)
 
-# Join the two events data tables (left join) with the end timestep determined by last timestep in the Manually identified turbidity file.
-CombEventsOwBED6ET0.89063ALG1<-OW061024_051124C_ManVer_TurbEvent%>%
-  filter(TIME_STEP<=last_timestep_CANARY) %>% # this filters the manually identified turbidity file based on the last TIME_STEP in the CANARY file
-  left_join(CANARYOwenmoreBED6ET0.89063Alg1, by="TIME_STEP") # left-join the CANARY file to the manually identified events file
+# Get list of directory names in the folder "07 EPA CANARY Event Detection"
+## Each directory is associated with a different configuration of CANARY.
+## Within each directory are output files generated by various versions of the algorithm.
+## The path for top_dir should be given relative to the project directory.
+output_file_top_directory<-"07 EPA CANARY Event Detection/Output_Files/"
 
-# View the first few rows and the structure of the combined dataframe
-head(CombEventsOwBED6ET0.89063ALG1)
-str(CombEventsOwBED6ET0.89063ALG1)
+output_file_full_path<-paste0(output_file_top_directory,data_set_identifier)
 
-# Export this data table as.csv file
-here() # indicates the working directory
-CombEventsOwBED6ET0.89063ALG1$TIME_STEP<-format(CombEventsOwBED6ET0.89063ALG1$TIME_STEP) # this prevents 00:00:00 being removed from Timestamps
+# List directories but not sub-directories.
+(directory_list<-list.dirs(output_file_full_path,recursive=FALSE))
 
-write.csv(CombEventsOwBED6ET0.89063ALG1, "08 Manually compare events to CANARY events/Output_Files_OWBED6ET0.89063/CombEventsOWBED6ET0.89063ALG1.csv", row.names=FALSE)
-
-# Load the other 15 algorithm files from 07 EPA CANARY Event Detection/Owenmore_LPCF_BED6_ET0.89063
-
-CANARYOwenmoreBED6ET0.89063Alg2<- fread("07 EPA CANARY Event Detection/Owenmore_LPCF_BED6_ET0.89063/Owenmore_LPCF_Var_HWV01.OW061024_051124C_ET0.89063-details-alg_2.csv", 
-               select = c("TIME_STEP","Contrib_{Turbidity}"))[, 
-               TIME_STEP := as.POSIXct(TIME_STEP, format = "%d/%m/%Y %H:%M", tz = "UTC")]
-
-CANARYOwenmoreBED6ET0.89063Alg3<- fread("07 EPA CANARY Event Detection/Owenmore_LPCF_BED6_ET0.89063/Owenmore_LPCF_Var_HWV01.OW061024_051124C_ET0.89063-details-alg_2.csv", 
-               select = c("TIME_STEP","Contrib_{Turbidity}"))[, 
-               TIME_STEP := as.POSIXct(TIME_STEP, format = "%d/%m/%Y %H:%M", tz = "UTC")]
-
-
-#CIAN - THIS NEEDS TO BE REPEATED FOR THE OTHER 15 x .csv files IN FOLDER '07 EPA CANARY Event Detection/Owenmore_LPCF_BED6_ET0.89063' WITH THE OUTPUT NAME ALG1, ALG2 etc UPDATED for
-# THE RELEVANT CANARYOwenmoreBED6ET0.89063Alg FILE.
-# IS THE BEST  APPROACH BY LOOPING THIS THROUGH THE FILES OR BETTER TO CREATE A FUNCTION AND APPLY THE FUNCTION TO EACH FILE?  IF YOU CAN ADVISE THE BEST APPROACH THEN I CAN APPLY
-# IT TO THE OTHER 5 FOLDERS OF ALGORITHMS, 96 IN TOTAL.
-
-# Create a list of Dataframes so the Owenmore CANARY files can be formatted and joined to OW061024_051124C_ManVer_TurbEvent
-BED6ET0.89063Alg1_16<-list(CANARYOwenmoreBED6ET0.89063Alg2, CANARYOwenmoreBED6ET0.89063Alg3)
-
-
-# Create a function to format the Owenmore CANARY files and join them to OW061024_051124C_ManVer_TurbEvent
-
-Join_OWCANARY_To_ManVer_TurbEvent<-function(df){
-# rename column
-df<df%>%
-    rename(CANARY_Contrib_Turbidity = "Contrib_{Turbidity}")%>%
-# Change any negative values in CANARYOwenmoreAlg1 Contrib_{Turbidity} to absolute values to facilitate matching query
-    mutate(`CANARY_Contrib_Turbidity`= abs(`CANARY_Contrib_Turbidity`))%>%
-# Join the two events data tables (left join)
-    left_join(OW061024_051124C_ManVer_TurbEvent, by="TIME_STEP")
-# Convert TIME_STEP to chr
-    mutate(TIME_STEP = as.character(TIME_STEP)) # this prevents 00:00:00 being removed from Timestamps
-}
-
-#  Apply the function to the list of dataframes, BED6ET0.89063Alg1_16 and export the new dataframes to Output_Files_OWBED6ET0.89063
-for (i in seq_along(BED6ET0.89063Alg1_16)) {
-  # Process the dataframe
-  new_df<-Join_OWCANARY_To_ManVer_TurbEvent(BED6ET0.89063Alg1_16[[i]])
-  # A new dataframe name is created based on the index i
-  new_df_name<-paste0("CombEventsOWBED6ET0.89063ALG", i)
-  # Assign the processed dataframe to an object
-  assign(new_df_name, new_df)
-  # The output file name
-  output_file<-paste0("08 Manually compare events to CANARY events/Output_Files_OWBED6ET0.89063/",new_df_name, ".csv")
+ID_String<-vector(mode="character",length=length(directory_list))
+for (i in 1:length(directory_list)){
   
-  # Write to csv
-  write.csv(Join_OWCANARY_To_ManVer_TurbEvent, file="08 Manually compare events to CANARY events/Output_Files_OWBED6ET0.89063", row.names=FALSE)
+  ID_String[i]<-paste0("_L",sub(".*_L","",directory_list[i]))
 }
- 
 
-#CIAN - I'VE TRIED CREATING A FUNCTION FOR THIS, IT MAY NOT BE THE BEST APPROACH AND I'M GETTING AN ERROR 
-# ' Error in Ops.data.frame(df, df %>% rename(CANARY_Contrib_Turbidity = "Contrib_{Turbidity}") %>%  : 
-#‘<’ only defined for equally-sized data frames
-
-#NOT SURE WHAT I'M DOING WRONG BUT OW061024_051124C_ManVer_TurbEvent IS THE MASTER DATAFILE AS SUCH. THE CANARY SOFTWARE INSERTS DATES & TIMES IF THE TIME SERIES IS NOT CONTINUOUS
-# THE SENSORS NEED TO BE REMOVED FROM THE RIVER AND CALIBRATED, SERVICED REGULARLY SO THERE IS A MISMATCH IN 
-# THE NUMBER OF OBSERVATIONS BETWEEN THE DATAFRAMES SO I NEED TO LEFT JOIN TO THIS SMALLER DATAFRAME AS IT IS THE MASTER ONE AND EXCLUDE THE DATES& TIMES canary INSERTS.  
-# IT DOESN'T SEEM TO BE AN ISSUE FOR LINE 40 OF THE CODE ABOVE WHEN DONE INDIVIDUALLY BUT LOOKS LIKE IT MAY BE AN ISSUE FOR FUNCTIONS OR I COULD BE READING THAT WRONG SO
-# ANY ADVICE YOU HAVE WOULD BE GREAT TO TROUBLESHOOT THIS.
-
-
+# Load the Owenmore CANARY file From 07 EPA CANARY Event Detection Folder
+## Now, step through each folder and locate the csv files for each algorithm.
+## Loop over the directories
+for (i in 1:length(directory_list)){
+  
+  # List all files in each parent directory, in turn.
+  (files_list<-list.files(directory_list[i],pattern='alg'))   #list all files in specified directory (directory_list[i]) that match pattern 'alg'.
+  alg_ver<-vector(mode="character",length=length(files_list)) # initialise two character vectors, alg_ver and Output_filename_string, to store information about the files.
+  Output_filename_string<-vector(mode="character",length=length(files_list))
+  
+  # Loop through each file in the files_list  
+  for (j in 1:length(files_list)){
+    
+    
+    # Put together the input filename and directory it is in.
+    input_filename_string<-paste0(directory_list[i],'/',files_list[j])
+    
+    # Read each of the "CANARY Event Detection" data files into a dataframe called "Input_Data"
+    Input_Data<-fread(input_filename_string, header=T, stringsAsFactors = F,
+                      select = c("TIME_STEP","P_Event"))
+    
+    
+    # Put together string name for output file.
+    
+    # First get the "algorithm version from each filename.
+    # It's found by taking the part of the filename after the word "details" without the last 5 characters.
+    
+    alg_ver[j]<-str_sub(sub(".*details-","",files_list[j]), end=-5)
+    
+    # Setup the output filename.
+    #(output_filename_string<-paste0("CANARYOwenmore",ID_String[i],alg_ver))
+    
+    #str(Input_Data)
+    
+    #Change TIME_STEP from chr to Date 
+    Input_Data$TIME_STEP<-as.POSIXct(Input_Data$TIME_STEP, format = "%d/%m/%Y %H:%M", tz="UTC")
+    
+    # Change any negative values in CANARYOwenmoreAlg1 P_Event to absolute values to facilitate matching query
+    Input_Data$`P_Event`<-abs(Input_Data$`P_Event`)
+    
+    
+    
+    # A column containing the "manually verified event data" is added to the "CANARY" dataframe and saved into a new...
+    #... data frame called "Combined Data".
+    
+    Combined_Data<-merge(Input_Data,ManVer_TurbEvent,by="TIME_STEP")
+    
+    #(which(is.na(Input_Data), arr.ind=TRUE))
+    
+    # View the first few rows and the structure of the combined dataframe
+    #head(Combined_Data)
+    #str(Combined_Data)
+    
+    # Export this data table as.csv file
+    here() # indicates the working directory
+    Combined_Data$TIME_STEP<-format(Combined_Data$TIME_STEP) # this prevents 00:00:00 being removed from Timestamps
+    
+    
+    # Now setup directory and filename for saving the combined data file. 
+    
+    Output_directory_string<-paste0("08 Manually compare events to CANARY events/Output_Files/",data_set_identifier)
+    Output_subdirectory_string<-paste0("Output_Files_",ID_String[i],"/")
+    
+    Output_directory_string_complete<-paste0(Output_directory_string,Output_subdirectory_string)
+    
+    # Extract the location and date range information from the "manually verified event data" filename.
+    # i.e, take the first part of the filename before the "_ManVer_TurbEvents...." 
+    
+    Location_and_Date_Range_ID_String<-sub("\\_M.*","",ManVer_file)
+    
+    Output_filename_string[j]<-paste0(Location_and_Date_Range_ID_String,ID_String[i],alg_ver[j],".csv")
+    
+    (Output_file_path_and_name<-paste0(Output_directory_string,Output_subdirectory_string,Output_filename_string[j]))
+    
+    if (!dir.exists(Output_directory_string_complete)) dir.create(Output_directory_string_complete, recursive = TRUE)
+    
+    write.csv(Combined_Data,Output_file_path_and_name, row.names=FALSE)
+    if (j==1){
+      # Only print this message once per processed directory.
+      cat("Writing data to file:\n",Output_file_path_and_name,"\n")}
+  }
+}
 
